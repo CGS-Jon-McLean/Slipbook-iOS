233076cf3a64722e4d085d09a00770d15841f6cf
Jon McLean <37740@cgs.act.edu.au>
2015-07-26 15:46:54 +1000

    Added Receipt Display Controller and Menu Segues

---
+++ Slipbook/DisplayViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DisplayViewController.swift
+//  Slipbook
+//
+//  Created by Jon on 26/07/2015.
+//  Copyright (c) 2015 Actro. All rights reserved.
+//
+
+import UIKit
+
+class DisplayViewController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
