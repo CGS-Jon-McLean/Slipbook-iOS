eaf8782a51f35ed54fc79973178820d32dec938f
Jon McLean <37740@cgs.act.edu.au>
2015-07-26 16:01:06 +1000

    Added menu display but menu doesn't load items from vc

--- Slipbook/DisplayViewController.swift
+++ Slipbook/DisplayViewController.swift
@@ -10,8 +10,17 @@ import UIKit
 
 class DisplayViewController: UITableViewController {
     
+    @IBOutlet var menuButton: UIBarButtonItem!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if self.revealViewController() != nil {
+            menuButton.target = revealViewController()
+            menuButton.action = "revealToggle:"
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
     }
     
     override func didReceiveMemoryWarning() {
