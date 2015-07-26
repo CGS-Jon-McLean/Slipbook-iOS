e14a997596082e0cb3095429c86b432e483787f8
Jon McLean <37740@cgs.act.edu.au>
2015-07-26 15:36:10 +1000

    Added Menu Controller

---
+++ Slipbook/MenuController.swift
@@ -0,0 +1,21 @@
+//
+//  MenuController.swift
+//  Slipbook
+//
+//  Created by Jon on 26/07/2015.
+//  Copyright (c) 2015 Actro. All rights reserved.
+//
+
+import UIKit
+
+class MenuController: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
