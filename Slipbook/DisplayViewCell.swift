5773642005baa7efd803d9bc2835230cb5a96b34
Jon McLean <37740@cgs.act.edu.au>
2015-07-26 22:33:37 +1000

    Added debug lines

---
+++ Slipbook/DisplayViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  DisplayViewCell.swift
+//  Slipbook
+//
+//  Created by Jon on 26/07/2015.
+//  Copyright (c) 2015 Actro. All rights reserved.
+//
+
+import UIKit
+
+class DisplayViewCell: UITableViewCell {
+    
+    
+    @IBOutlet var name: UILabel!
+    
+    func addCell(nameString: String) {
+        name.text = nameString
+    }
+    
+}
