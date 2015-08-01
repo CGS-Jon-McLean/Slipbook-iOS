610d4125127a809bc15aaf72111079f0ef4e8bf8
Ben McLean <ben.mclean@cgs.act.edu.au>
2015-08-01 13:15:44 +1000

    Added network class

---
+++ Slipbook/Networking.swift
@@ -0,0 +1,26 @@
+//
+//  Networking.swift
+//  Slipbook
+//
+//  Created by BenM on 1/08/2015.
+//  Copyright (c) 2015 Actro. All rights reserved.
+//
+
+import Foundation
+
+class Networking{
+    
+    func getResult(urlPath:String) -> NSDictionary {
+        var url: NSURL = NSURL(string: urlPath)!
+        var request1: NSURLRequest = NSURLRequest(URL: url)
+        var response: AutoreleasingUnsafeMutablePointer<NSURLResponse? >= nil
+        var error: NSErrorPointer = nil
+        var dataVal: NSData =  NSURLConnection.sendSynchronousRequest(request1, returningResponse: response, error:nil)!
+        var err: NSError
+        println(response)
+        var jsonResult: NSDictionary = NSJSONSerialization.JSONObjectWithData(dataVal, options: NSJSONReadingOptions.MutableContainers, error: nil) as NSDictionary
+        println("Synchronous \(jsonResult)")
+        return jsonResult
+    }
+    
+}
