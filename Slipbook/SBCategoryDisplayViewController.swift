c623d3456c518fa061831e9379b13ab27841144a
Jon McLean <37740@cgs.act.edu.au>
2015-08-16 19:43:46 +1000

    Added Category Class for Ben to work on.

---
+++ Slipbook/SBCategoryDisplayViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CategoryDisplayViewController.swift
+//  Slipbook
+//
+//  Created by Jon on 16/08/2015.
+//  Copyright (c) 2015 Actro. All rights reserved.
+//
+
+import UIKit
+
+class SBCategoryDisplayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
